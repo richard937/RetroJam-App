844d4e7e22e39e3616cf3ab5a45f29295a040f30
richard937 <papuncharan@gmail.com>
2019-12-30 16:43:54 +0530

    Added all the files

---
+++ drum.js
@@ -0,0 +1,20 @@
+// alert("connected");
+
+function playSound(e){
+let audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
+let key=document.querySelector(`.key[data-key="${e.keyCode}"]`);
+if(!audio) return;
+audio.currentTime = 0;
+audio.play();
+key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+
+let keys= document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+window.addEventListener('keydown',playSound);
